9765f38b283f5a2dc412ea1df571dcfafbc931f7
KeithLion <nortonkiara6@gmail.com>
2021-12-24 20:16:07 -0800

    added the routes that will add to the counter everytime the page is refreshed and the route that will clear the session

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, session, redirect
+
+app = Flask(__name__)
+app.secret_key = 'the count'
+
+
+@app.route('/')
+def counter():
+    if 'count' not in session:
+        session['count'] = 0
+    else:
+        session['count'] += 1
+        return render_template('count.html')
+
+
+@app.route('/add2', method=['post'])
+def add():
+    session['count'] += 1
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
